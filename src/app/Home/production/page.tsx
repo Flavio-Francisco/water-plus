f967e932b2d29d9f472bef1f20e5a4bd20e67771
Flavio Francisco da Silva <111377254+Flavio-Francisco@users.noreply.github.com>
2024-03-04 12:28:06 +0000

    STARTING THE WEEK

--- src/app/Home/production/page.tsx
+++ src/app/Home/production/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import GraficProduction from "@/components/graficProtution";
-import FormApvisa from "@/components/formApvisa";
 import Line from "@/components/line";
 import { dataProducao, calcularPorcentagem } from "@/utils/models/Data";
 import { Row } from "react-bootstrap";
@@ -26,7 +25,7 @@ export default function Production() {
       <div className="d-flex justify-content-center align-items-center mt-3">
         <p>Taxa de rejeito {percentage} </p>
       </div>
-      <FormApvisa />
+      
     </div>
   );
 }
