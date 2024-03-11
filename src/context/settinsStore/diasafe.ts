13e2f7332575d22b3354ece3c086493eab2ec7be
Flavio <flavyosilva@hotmail.com>
2024-03-11 18:35:58 -0300

    create diasafeReducer

---
+++ src/context/settinsStore/diasafe.ts
@@ -0,0 +1,24 @@
+import { DiasafeModel } from "../../utils/models/diasafe";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface DiasafeState {
+  diasafe: DiasafeModel[];
+}
+
+const initialState: DiasafeState = {
+  diasafe: [],
+};
+
+const diasafeStatetSlice = createSlice({
+  name: "users",
+  initialState,
+  reducers: {
+    addUser(state, action: PayloadAction<DiasafeModel>) {
+      state.diasafe.push(action.payload);
+    },
+    // Outras reducers relacionadas aos usuários
+  },
+});
+
+export const { addUser } = diasafeStatetSlice.actions;
+export default diasafeStatetSlice.reducer;
--- src/context/store.ts
+++ src/context/store.ts
@@ -1,20 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit'
-import analysisReducer from './settinsStore/analysisReducer'
-import usersReducer from './settinsStore/userReducer'
-import apevisaReducer from './settinsStore/userReducer'
-import chemistReducer from './settinsStore/chemistReducer'
+import { configureStore } from "@reduxjs/toolkit";
+import analysisReducer from "./settinsStore/analysisReducer";
+import usersReducer from "./settinsStore/userReducer";
+import apevisaReducer from "./settinsStore/userReducer";
+import chemistReducer from "./settinsStore/chemistReducer";
+import diasafeReducer from "./settinsStore/diasafe";
+
 export const store = configureStore({
   reducer: {
-   analysis: analysisReducer,
-   users: usersReducer,
-   apevisa:apevisaReducer,
-   chemist :chemistReducer 
+    analysis: analysisReducer,
+    users: usersReducer,
+    apevisa: apevisaReducer,
+    chemist: chemistReducer,
+    diasafe: diasafeReducer,
   },
-})
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
-
-
+export type AppDispatch = typeof store.dispatch;
