f47477255df5dfb70861d7718ce149b89e8ea190
Flavio-Franciso <flavyosilva@hotmail.com>
2024-07-24 10:45:10 -0300

    refatore status route post level

--- src/app/api/level/route.ts
+++ src/app/api/level/route.ts
@@ -13,11 +13,14 @@ export async function POST(req: NextRequest) {
       
       // Verifica se 'id' é igual a "1"
       if (id === "1") {
-        return NextResponse.json(jsonData);
+        return NextResponse.json(jsonData,{status:200});
       } else {
         return NextResponse.json({
           message: "ID não reconhecido",
           id: id
+        },
+          {
+          status:200
         });
       }
       
