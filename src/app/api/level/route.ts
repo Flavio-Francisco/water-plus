2471876bf386395713bce98308c4d18084f221cf
Flavio-Franciso <flavyosilva@hotmail.com>
2024-07-23 21:31:08 -0300

    refatore level

--- src/app/api/level/route.ts
+++ src/app/api/level/route.ts
@@ -2,27 +2,27 @@
 import { NextRequest, NextResponse } from "next/server";
 
 
-
-
-
-
 export async function POST(req: NextRequest) {
     const url = new URL(req.nextUrl.href);
     const id = url.searchParams.get("id");
-    
+  
     try {
-   
-      if (id ==="1") {
+      if (id === "1") {
         return NextResponse.json(req.json()); 
-      }
-
-        
-    } catch (error) {
+      } else {
         return NextResponse.json({
-            message: "erro ao registrar"
-        },
-        {
-            status: 500
+          message: "ID não reconhecido",
+          id: id
         });
+      }
+    } catch (error) {
+      return NextResponse.json({
+        message: "Erro ao processar requisição",
+        error: error
+      },
+      {
+        status: 500
+      });
     }
-}
+  }
+  
