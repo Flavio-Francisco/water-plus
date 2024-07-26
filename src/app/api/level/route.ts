d4ff840292ed9ce65a1381114f4f5e8c9d2bf390
Flavio-Franciso <flavyosilva@hotmail.com>
2024-07-26 10:24:28 -0300

    reate UPDATE DELETE POST

--- src/app/api/level/route.ts
+++ src/app/api/level/route.ts
@@ -8,9 +8,33 @@ interface Level{
   system: string
   
 }
+ 
 
+export async function GET(req: NextRequest) {
+  const url = new URL(req.nextUrl.href);
+  const id = url.searchParams.get("id");
+  
+  try {
+    const level = await prisma.level.findMany(
+      {
+        where:
+        {
+        
+          system_id:Number(id)
+        }
+      })
+      return NextResponse.json(level);
+  } catch (error) {
+    console.error('Error:', error);
+    return NextResponse.json({
+        message: "Erro ao processar requisição",
+        error: error
+    }, {
+        status: 500
+    });
+  }
 
-
+}
 export async function POST(req: NextRequest) {
   const url = new URL(req.nextUrl.href);
   const id = url.searchParams.get("id");
@@ -29,6 +53,40 @@ export async function POST(req: NextRequest) {
               level: data.level,
               system_id: Number(id)
           }
+      });
+      
+      return NextResponse.json(level);
+  } catch (error) {
+      console.error('Error:', error);
+      return NextResponse.json({
+          message: "Erro ao processar requisição",
+          error: error
+      }, {
+          status: 500
+      });
+  }
+}
+
+export async function PATCH(req: NextRequest) {
+  const url = new URL(req.nextUrl.href);
+  const id = url.searchParams.get("id");
+  
+  try {
+ 
+    const data:Level = await req.json();
+
+
+    const level = await prisma.level.update({
+        
+      where: {
+        id:data.id,
+        system_id: Number(id)
+      },
+
+      data: {
+        level:data.level
+      }
+     
       });
       
       return NextResponse.json(level);
