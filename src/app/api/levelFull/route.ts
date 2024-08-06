02ac66b13f95dca63e38a26c29048bc59449fc6d
Flavio-Franciso <flavyosilva@hotmail.com>
2024-08-06 10:01:23 -0300

    refatore route levelFull

--- src/app/api/levelFull/route.ts
+++ src/app/api/levelFull/route.ts
@@ -11,10 +11,54 @@ const corsHeaders = {
 export async function GET(req: NextRequest) {
   const url = new URL(req.nextUrl.href);
   const id = url.searchParams.get("id");
-const pointName = await req.json()
+
   if (req.method === 'OPTIONS') {
     return new NextResponse(null, { headers: corsHeaders });
   }
+
+  try {
+    const levels = await prisma.level.findMany({
+      where: {
+        system_id: Number(id),
+        id: {
+          not: 4
+        },
+        pointName: {
+          not: null  // Adicionado filtro para `pointName` não ser nulo
+        }
+      },
+      orderBy: {
+        id: 'desc'  // Ordenar por ID, assumindo que IDs maiores foram criados mais recentemente
+      },
+     
+      select: {
+        pointName: true  // Selecionar apenas o campo `pointName`
+      }
+    });
+
+    const uniquePointNames = Array.from(new Set(levels.map(level => level.pointName)));
+
+    return NextResponse.json(uniquePointNames, { headers: corsHeaders });
+  } catch (error) {
+    console.error('Error:', error);
+    return NextResponse.json({
+      message: "Erro ao processar requisição",
+      error: error
+    }, {
+      status: 500,
+      headers: corsHeaders
+    });
+  }
+}
+
+export async function POST(req: NextRequest) {
+  const url = new URL(req.nextUrl.href);
+  const id = url.searchParams.get("id");
+const {pointName }= await req.json()
+  if (req.method === 'OPTIONS') {
+    return new NextResponse(null, { headers: corsHeaders });
+  }
+console.log(pointName);
 
   try {
     const levels = await prisma.level.findMany({
@@ -42,6 +86,5 @@ const pointName = await req.json()
     });
   }
 }
-
 
 
