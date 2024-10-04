e91b122152528750ff7c3d83f2b1ea0d66695505
Flavio-Franciso <flavyosilva@hotmail.com>
2024-10-04 11:06:46 -0300

    create post system

--- src/app/api/systems/route.ts
+++ src/app/api/systems/route.ts
@@ -1,5 +1,5 @@
 
-import {NextResponse } from "next/server";
+import {NextRequest, NextResponse } from "next/server";
 import prisma from "../../../../lib/db";
 import { Systems } from "@/utils/models/analysis";
 
@@ -9,6 +9,35 @@ export async function GET() {
     
     try {
         const data:Systems[] = await prisma.systems.findMany()
+
+        return NextResponse.json(data)
+    } catch (error) {
+        return NextResponse.json({
+            message: "Usuário não encontado"
+        },
+            {
+                status: 500
+            })
+    }
+}
+
+export async function POST(request:NextRequest) {
+   
+    const system: Systems = await request.json();
+  
+    try {
+        const data = await prisma.systems.create({
+            data: {
+                name: system.name||"",
+                address: system.address,
+                city: system.city,
+                state: system.state,
+                zipCode: system.zipCode,
+                number: system.number,
+             
+                
+            }
+        })
 
         return NextResponse.json(data)
     } catch (error) {
